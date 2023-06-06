3b2606a6a6609ceda3e4c3ee1fab50b1f58ab632
Nishmal <nishmal.yt.3@gmail.com>
2023-06-06 11:18:30 +0530

    change route

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
    
     <Router>  
       <Routes>
-         <Route path="/home" element={<Home/>}/>
+         <Route path="/" element={<Home/>}/>
          <Route path="/about" element={<About/>}/>
          <Route path="/prebook" element={<PreBook/>}/>
          <Route path="/login" element={<LoginPage/>}/>
